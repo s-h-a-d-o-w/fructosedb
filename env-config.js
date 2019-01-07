02d7c73cab573bb8c121a0bc4f4089216f6f4eda
Andreas Opferkuch <s-h-a-d-o-w@users.noreply.github.com>
2019-01-07 13:51:54 +0100

    ip.address() - not reliable if there are multiple network interfaces

--- env-config.js
+++ env-config.js
@@ -1,5 +1,31 @@
 const dotenv = require('dotenv');
-const ip = require('ip');
+const os = require('os');
+
+function getLocalIP() {
+	// Network interfaces we want to ignore
+	const blacklist = [/VirtualBox/];
+
+	function inBlacklist(key) {
+		return blacklist.some((regex) => key.match(regex) !== null);
+	}
+
+	const interfaces = os.networkInterfaces();
+	let retval;
+	Object.keys(interfaces)
+		.filter((key) => !inBlacklist(key))
+		.some((key) => {
+			let ipv4 = interfaces[key].find((address) => address.family === 'IPv4');
+
+			if (ipv4 !== undefined) {
+				retval = ipv4.address;
+				return true;
+			}
+
+			return false;
+		});
+
+	return retval;
+}
 
 // In production, environment variables come from cloud
 // (via now.json and secrets)
@@ -9,7 +35,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 process.env.PORT = parseInt(process.env.PORT, 10) || 3000;
 process.env.BACKEND_URL = !process.env.BACKEND_URL
-	? `http://${ip.address()}:${process.env.PORT}`
+	? `http://${getLocalIP()}:${process.env.PORT}`
 	: process.env.BACKEND_URL;
 
 // Exported values are provided to frontend via transform-define
