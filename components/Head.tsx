702b9a656c34c70db31fa01488d22155acb25e95
Andreas Opferkuch <s-h-a-d-o-w@users.noreply.github.com>
2019-04-30 23:29:00 +0200

    Minor refactoring of Head, add www-less alias

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,25 +1,23 @@
 import * as React from 'react';
-import Head from 'next/head';
+import NextHead from 'next/head';
 
-export default class extends React.PureComponent {
-	render() {
-		return (
-			<Head>
-				<title>fructosedb - find low fructose foods</title>
-				<meta
-					name="Description"
-					content="A list of foods with their fructose and glucose content that allows people
-					suffering from fructose malabsorption (also sometimes referred to as &quot;fructose intolerance&quot; or
-					&quot;DFI&quot;) to adjust their diet more easily."
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="theme-color" content="#653399" />
-				<link rel="manifest" href="/site.webmanifest" />
-				<link rel="stylesheet" href="/static/css/global.css" />
-				{/* Force favicon refresh on changes */}
-				<link rel="icon" type="image/x-icon" href="/favicon.ico?v=20181018" />
-				<link rel="prefetch" href="/static/images/usda-symbol.svg" />
-			</Head>
-		);
-	}
+export default function Head() {
+	return (
+		<NextHead>
+			<title>fructosedb - find low fructose foods</title>
+			<meta
+				name="Description"
+				content='A list of foods with their fructose and glucose content that allows people
+					suffering from fructose malabsorption (also sometimes referred to as "fructose intolerance" or
+					"DFI") to adjust their diet more easily.'
+			/>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta name="theme-color" content="#653399" />
+			<link rel="manifest" href="/site.webmanifest" />
+			<link rel="stylesheet" href="/static/css/global.css" />
+			{/* Force favicon refresh on changes */}
+			<link rel="icon" type="image/x-icon" href="/favicon.ico?v=20181018" />
+			<link rel="prefetch" href="/static/images/usda-symbol.svg" />
+		</NextHead>
+	);
 }
